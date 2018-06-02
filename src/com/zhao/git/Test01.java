8df45b0f19597c73bcf4b48741bba3e660086a3d
javazja <java_zja@126.com>
2018-06-02 22:10:42 +0800

    update file 02

--- src/com/zhao/git/Test01.java
+++ src/com/zhao/git/Test01.java
@@ -3,5 +3,6 @@ package com.zhao.git;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello github server!!");
+		System.out.println("hello github server!! 02");
 	}
 }
