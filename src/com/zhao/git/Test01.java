2c92033c553035ea30fc003fc50c8b77f835cb21
mywlzja <35247995+mywlzja@users.noreply.github.com>
2018-06-02 22:15:55 +0800

    Update Test01.java

--- src/com/zhao/git/Test01.java
+++ src/com/zhao/git/Test01.java
@@ -4,5 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello github server!!");
 		System.out.println("hello github server!! 02");
+		//git hub update 01
+		System.out.println("hello github server!! 03");
 	}
 }
